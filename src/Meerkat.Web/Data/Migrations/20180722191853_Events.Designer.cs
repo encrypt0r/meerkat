2b9e61dc1b5e4769932a9bd63e3041caf7ee56f5
Muhammad Azeez <muhammad-azeez@outlook.com>
2018-07-22 23:31:52 +0300

    create migrations

--- src/Meerkat.Web/Data/Migrations/20180722191853_Events.Designer.cs
+++ src/Meerkat.Web/Data/Migrations/20180722191853_Events.Designer.cs
@@ -4,14 +4,16 @@ using Meerkat.Web.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Meerkat.Web.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180722191853_Events")]
+    partial class Events
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/Meerkat.Web/Data/Migrations/20180722191853_Events.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Meerkat.Web.Data.Migrations
+{
+    public partial class Events : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Events",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true),
+                    Message = table.Column<string>(nullable: true),
+                    Date = table.Column<DateTime>(nullable: false),
+                    Username = table.Column<string>(nullable: true),
+                    Level = table.Column<int>(nullable: false),
+                    Release = table.Column<string>(nullable: true),
+                    RootCause = table.Column<string>(nullable: true),
+                    Type = table.Column<string>(nullable: true),
+                    MachineName = table.Column<string>(nullable: true),
+                    OperatingSystem = table.Column<string>(nullable: true),
+                    Runtime = table.Column<string>(nullable: true),
+                    Modules = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Events", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Frames",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Function = table.Column<string>(nullable: true),
+                    Module = table.Column<string>(nullable: true),
+                    ContextLine = table.Column<string>(nullable: true),
+                    InApp = table.Column<bool>(nullable: false),
+                    LineNumber = table.Column<int>(nullable: false),
+                    ColumnNumber = table.Column<int>(nullable: false),
+                    FileName = table.Column<string>(nullable: true),
+                    EventId = table.Column<long>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Frames", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Frames_Events_EventId",
+                        column: x => x.EventId,
+                        principalTable: "Events",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Frames_EventId",
+                table: "Frames",
+                column: "EventId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Frames");
+
+            migrationBuilder.DropTable(
+                name: "Events");
+        }
+    }
+}
